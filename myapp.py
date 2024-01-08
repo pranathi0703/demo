6d2f8716c0f85cf65b0e85170316ef35bfc858af
pranathi0703 <132868868+pranathi0703@users.noreply.github.com>
2024-01-08 11:55:47 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,9 +4,9 @@ st.header("Types of dogs")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("French Bulldogs")
-  st.image("C:\Users\LENOVO\Downloads\French bulldog.jpg",caption="French Bulldogs",width=300,use_column_width=True)
+  st.image("./French bulldog.jpg",caption="French Bulldogs",width=300,use_column_width=True)
   st.write("French Bulldogs are cute")
   with col2:
   st.subheader("Golden Retriver")
-  st.image("C:\Users\LENOVO\Downloads\golden retriver.jpg",caption="Golden Retriver",width=300,use_column_width=True)
+  st.image("./golden retriver.jpg",caption="Golden Retriver",width=300,use_column_width=True)
   st.write("Golden Retriver dogs are cute")
