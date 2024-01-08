d5b4ec4e588568dfa6a007aaf2ac914217407ff2
pranathi0703 <132868868+pranathi0703@users.noreply.github.com>
2024-01-08 11:59:08 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -6,7 +6,7 @@ with col1:
   st.subheader("French Bulldogs")
   st.image("./French bulldog.jpg",caption="French Bulldogs",width=300,use_column_width=True)
   st.write("French Bulldogs are cute")
-  with col2:
+with col2:
   st.subheader("Golden Retriver")
   st.image("./golden retriver.jpg",caption="Golden Retriver",width=300,use_column_width=True)
   st.write("Golden Retriver dogs are cute")
