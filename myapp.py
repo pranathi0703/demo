b98346d195a173318f082312c7f0c86c35e17879
pranathi0703 <132868868+pranathi0703@users.noreply.github.com>
2024-01-08 11:42:24 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,1 +1,12 @@
-
+import streamlit as st
+st.set_page_config(page_title='Cats')
+st.header("Types of dogs")
+col1,col2=st.columns(2)
+with col1:
+  st.subheader("French Bulldogs")
+  st.image("C:\Users\LENOVO\Downloads\French bulldog.jpg",caption="French Bulldogs",width=300,use_column_width=True)
+  st.write("French Bulldogs are cute")
+  with col2:
+  st.subheader("Golden Retriver")
+  st.image("C:\Users\LENOVO\Downloads\golden retriver.jpg",caption="Golden Retriver",width=300,use_column_width=True)
+  st.write("Golden Retriver dogs are cute")
